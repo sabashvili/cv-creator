1b6d45af57d933d0086ee4006ca6f804337ffb7d
sabashvili <sabashvili.shota@gmail.com>
2023-03-13 00:16:32 +0400

    fix some bugs

--- personal-info/script.js
+++ personal-info/script.js
@@ -1,12 +1,13 @@
-backBtn = document.getElementById("go-to-restart");
+const backBtn = document.getElementById("go-to-restart");
+const allInputs = document.querySelectorAll("input, textarea");
+
 backBtn.addEventListener("click", function (e) {
   localStorage.clear();
 });
 
-const allInputs = document.querySelectorAll("input, textarea");
 for (const i of allInputs) {
   i.addEventListener("blur", function (e) {
     localStorage.setItem(i.name, i.value);
   });
-  document.getElementById(i.name).value = localStorage.getItem(i.name);
+  i.value = localStorage.getItem(i.name);
 }
