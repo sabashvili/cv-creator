485e1169e3d5db03046ae7d579fc511237eba7f2
Shota Sabashvili <s.sabashvili@ggtc.ge>
2023-03-13 16:37:02 +0400

    Set update resume option

--- personal-info/script.js
+++ personal-info/script.js
@@ -1,13 +1,33 @@
 const backBtn = document.getElementById("go-to-restart");
 const allInputs = document.querySelectorAll("input, textarea");
+const emailLogo = document.getElementById("email-logo")
+const phoneLogo = document.getElementById("phone-logo")
+const lableAboutMe = document.getElementById("label-about-me")
+
+function updateResume(i) {
+  if (i.name == "cv-email" && i.value != "") {
+    emailLogo.classList.remove("hidden")
+  } if (i.name == "cv-number" && i.value != "") {
+    phoneLogo.classList.remove("hidden")
+  } if (i.name == "cv-about-me" && i.value != "") {
+    lableAboutMe.classList.remove("hidden")
+  }
+  document.getElementById(i.name).textContent = localStorage.getItem(i.name);
+}
+
 
 backBtn.addEventListener("click", function (e) {
   localStorage.clear();
 });
 
 for (const i of allInputs) {
+  i.value = localStorage.getItem(i.name);
+  updateResume(i);
   i.addEventListener("blur", function (e) {
     localStorage.setItem(i.name, i.value);
+    updateResume(i);
   });
-  i.value = localStorage.getItem(i.name);
+
 }
+
+
