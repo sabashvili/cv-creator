ad1aa97d9ca6676b96d60df066f2a9d529625334
sabashvili <sabashvili.shota@gmail.com>
2023-03-13 22:17:36 +0400

    for bug fix

--- personal-info/script.js
+++ personal-info/script.js
@@ -1,43 +1,65 @@
 const backBtn = document.getElementById("go-to-restart");
 const allInputs = document.querySelectorAll("input, textarea");
-const emailLogo = document.getElementById("email-logo")
-const phoneLogo = document.getElementById("phone-logo")
-const lableAboutMe = document.getElementById("label-about-me")
+const emailLogo = document.getElementById("email-logo");
+const phoneLogo = document.getElementById("phone-logo");
+const lableAboutMe = document.getElementById("label-about-me");
+const persEndLine = document.getElementById("pers-end-line");
+const photo = document.getElementById("personal-photo");
 
 function updateResume(i) {
   if (i.name == "cv-email" && i.value != "") {
-    emailLogo.classList.remove("hidden")
+    emailLogo.classList.remove("hidden");
   } else if (i.name == "cv-email" && i.value == "") {
-    emailLogo.classList.add("hidden")
+    emailLogo.classList.add("hidden");
   }
 
   if (i.name == "cv-number" && i.value != "") {
-    phoneLogo.classList.remove("hidden")
+    phoneLogo.classList.remove("hidden");
   } else if (i.name == "cv-number" && i.value == "") {
-    phoneLogo.classList.add("hidden")
+    phoneLogo.classList.add("hidden");
   }
 
   if (i.name == "cv-about-me" && i.value != "") {
-    lableAboutMe.classList.remove("hidden")
+    lableAboutMe.classList.remove("hidden");
+    persEndLine.classList.remove("hidden");
   } else if (i.name == "cv-about-me" && i.value == "") {
-    lableAboutMe.classList.add("hidden")
+    lableAboutMe.classList.add("hidden");
+    persEndLine.classList.add("hidden");
   }
-  document.getElementById(i.name).textContent = localStorage.getItem(i.name);
+  if (i.name == "cv-photo") {
+    const img = localStorage.getItem(i.name);
+    if (img) {
+      document.getElementById(i.name).src = localStorage.getItem(i.name);
+    }
+  } else {
+    document.getElementById(i.name).textContent = localStorage.getItem(i.name);
+  }
 }
-
 
 backBtn.addEventListener("click", function (e) {
   localStorage.clear();
 });
 
 for (const i of allInputs) {
-  i.value = localStorage.getItem(i.name);
   updateResume(i);
-  i.addEventListener("blur", function (e) {
-    localStorage.setItem(i.name, i.value);
-    updateResume(i);
-  });
-
+  if (i.name != "cv-photo") {
+    i.value = localStorage.getItem(i.name);
+    i.addEventListener("blur", function (e) {
+      localStorage.setItem(i.name, i.value);
+      updateResume(i);
+    });
+  }
 }
 
+let selectedImage = "";
 
+photo.addEventListener("change", () => {
+  const fr = new FileReader();
+  fr.readAsDataURL(photo.files[0]);
+  fr.addEventListener("load", () => {
+    const url = fr.result;
+    selectedImage = url;
+    localStorage.setItem("cv-photo", url);
+    document.getElementById("cv-photo").src = url;
+  });
+});
