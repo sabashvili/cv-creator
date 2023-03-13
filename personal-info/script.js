4e8aa904e187009c25812251e92285e988f96bbd
Demetre Tetunashvili <demetre.tetunashvili@webiz.ge>
2023-03-13 22:43:03 +0400

    Fix bugs while updating resume

--- personal-info/script.js
+++ personal-info/script.js
@@ -7,32 +7,40 @@ const persEndLine = document.getElementById("pers-end-line");
 const photo = document.getElementById("personal-photo");
 
 function updateResume(i) {
-  if (i.name == "cv-email" && i.value != "") {
-    emailLogo.classList.remove("hidden");
-  } else if (i.name == "cv-email" && i.value == "") {
-    emailLogo.classList.add("hidden");
+  const value = localStorage.getItem(i.name);
+
+  if (i.name == "cv-email") {
+    if (value) {
+      emailLogo.classList.remove("hidden");
+    } else {
+      emailLogo.classList.add("hidden");
+    }
   }
 
-  if (i.name == "cv-number" && i.value != "") {
-    phoneLogo.classList.remove("hidden");
-  } else if (i.name == "cv-number" && i.value == "") {
-    phoneLogo.classList.add("hidden");
+  if (i.name == "cv-number") {
+    if (value) {
+      phoneLogo.classList.remove("hidden");
+    } else {
+      phoneLogo.classList.add("hidden");
+    }
   }
 
-  if (i.name == "cv-about-me" && i.value != "") {
-    lableAboutMe.classList.remove("hidden");
-    persEndLine.classList.remove("hidden");
-  } else if (i.name == "cv-about-me" && i.value == "") {
-    lableAboutMe.classList.add("hidden");
-    persEndLine.classList.add("hidden");
+  if (i.name == "cv-about-me") {
+    if (value) {
+      lableAboutMe.classList.remove("hidden");
+      persEndLine.classList.remove("hidden");
+    } else {
+      lableAboutMe.classList.add("hidden");
+      persEndLine.classList.add("hidden");
+    }
   }
+
   if (i.name == "cv-photo") {
-    const img = localStorage.getItem(i.name);
-    if (img) {
-      document.getElementById(i.name).src = localStorage.getItem(i.name);
+    if (value) {
+      document.getElementById(i.name).src = value
     }
   } else {
-    document.getElementById(i.name).textContent = localStorage.getItem(i.name);
+    document.getElementById(i.name).textContent = value
   }
 }
 
