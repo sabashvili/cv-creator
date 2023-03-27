2c6daddbd297b83722e017b9c1d75a0a9f9c26c3
Shota Sabashvili <s.sabashvili@ggtc.ge>
2023-03-27 16:01:36 +0400

    hidden div features

--- experience-info/script.js
+++ experience-info/script.js
@@ -18,43 +18,29 @@ let inputsArr = [];
 
 // Clear localStorage on back button
 backBtn.addEventListener("click", function (e) {
-  localStorage.clear();
+    localStorage.clear();
 });
 
 // Update previus (Personal information) CV.
 for (const i of allResumeDiv) {
-  const localStorageGetItem = localStorage.getItem(i.id);
-  if (localStorageGetItem) {
-    if (i.id != "cv-photo") {
-      i.textContent = localStorageGetItem;
-    } else {
-      document.getElementById(i.id).src = localStorageGetItem;
+    const localStorageGetItem = localStorage.getItem(i.id);
+    if (localStorageGetItem) {
+        if (i.id != "cv-photo") {
+            i.textContent = localStorageGetItem;
+        } else {
+            document.getElementById(i.id).src = localStorageGetItem;
+        }
     }
-  }
 }
 
 addMoreExperience.addEventListener("click", function (e) {
-  extraExperienceFormCounter++;
-  duplicateForm(extraExperienceFormCounter);
-  duplicateOutput(extraExperienceFormCounter);
-  localStorage.setItem("extraExperienceFormCounter", extraExperienceFormCounter);
+    extraExperienceFormCounter++;
+    duplicateForm(extraExperienceFormCounter);
+    duplicateOutput(extraExperienceFormCounter);
+    localStorage.setItem("extraExperienceFormCounter", extraExperienceFormCounter);
 });
 
-// if (i.name == "cv-position") {
-//     if (i.value) {
-//       positionComma.classList.remove("hidden");
-//     } else {
-//       positionComma.classList.add("hidden");
-//     }
-//   }
 
-//   if (i.name == "cv-end-work") {
-//     if (i.value) {
-//       workTimeConection.classList.remove("hidden");
-//     } else {
-//       workTimeConection.classList.add("hidden");
-//     }
-//   }
 
 //   //Experience text show and hide
 //   booleanObj[i.name] = Boolean(i.value);
@@ -73,59 +59,85 @@ addMoreExperience.addEventListener("click", function (e) {
 //   }
 
 function addBlurs(inputElements) {
-  for (const element of inputElements) {
-    element.value = localStorage.getItem(element.name);
-    updateResume(element);
-    element.addEventListener("blur", function () {
-      localStorage.setItem(element.name, element.value);
-      updateResume(element);
-    });
-  }
+    for (const element of inputElements) {
+        element.value = localStorage.getItem(element.name);
+        updateResume(element);
+        element.addEventListener("blur", function () {
+            localStorage.setItem(element.name, element.value);
+            updateResume(element);
+        });
+    }
 }
 
 extraExperienceFormCounter = localStorage.getItem("extraExperienceFormCounter");
 for (let i = 0; i < extraExperienceFormCounter; i++) {
-  const newFormFields = duplicateForm(i + 1);
-  duplicateOutput(i + 1);
+    const newFormFields = duplicateForm(i + 1);
+    duplicateOutput(i + 1);
 
-  addBlurs(newFormFields);
+    addBlurs(newFormFields);
 }
 
 addBlurs(startInputElement);
 
 function duplicateForm(counter) {
-  const originalFormSet = document.getElementById("experience-information-form-conteiner-0");
-  const newFormSet = document.createElement("div");
-  newFormSet.innerHTML = originalFormSet.innerHTML;
+    const originalFormSet = document.getElementById("experience-information-form-conteiner-0");
+    const newFormSet = document.createElement("div");
+    newFormSet.innerHTML = originalFormSet.innerHTML;
 
-  newFormSet.id = `experience-information-form-conteiner-${counter}`;
+    newFormSet.id = `experience-information-form-conteiner-${counter}`;
 
-  const newFormSetInputs = newFormSet.querySelectorAll("input, textarea");
-  for (const newFormElement of newFormSetInputs) {
-    newFormElement.name = `${newFormElement.name}-${counter}`;
-    newFormElement.addEventListener("blur", function () {
-      localStorage.setItem(newFormElement.name, newFormElement.value);
-      updateResume(newFormElement);
-    });
-  }
-  experienceFormChildren.appendChild(newFormSet);
-  return newFormSetInputs;
+    const newFormSetInputs = newFormSet.querySelectorAll("input, textarea");
+    for (const newFormElement of newFormSetInputs) {
+        newFormElement.name = `${newFormElement.name}-${counter}`;
+        newFormElement.addEventListener("blur", function () {
+            localStorage.setItem(newFormElement.name, newFormElement.value);
+            updateResume(newFormElement);
+        });
+    }
+
+    experienceFormChildren.appendChild(newFormSet);
+
+    return newFormSetInputs;
 }
 
 function duplicateOutput(counter) {
-  const originalOutoutSet = document.getElementById("experience-info-output-container-0");
-  const newOutputSet = document.createElement("div");
-  newOutputSet.innerHTML = originalOutoutSet.innerHTML;
-  newOutputSet.id = `experience-info-output-container-${counter}`;
-  const newOutputSetResume = newOutputSet.getElementsByClassName("cv-field");
-  for (const newOutputElement of newOutputSetResume) {
-    newOutputElement.id = `${newOutputElement.id}-${counter}`;
-    newOutputElement.textContent = "";
-  }
-  experienceOutput.appendChild(newOutputSet);
+    const originalOutoutSet = document.getElementById("experience-info-output-container-0");
+    const newOutputSet = document.createElement("div");
+    newOutputSet.innerHTML = originalOutoutSet.innerHTML;
+    newOutputSet.id = `experience-info-output-container-${counter}`;
+    const newOutputSetResume = newOutputSet.getElementsByClassName("cv-field");
+    for (const newOutputElement of newOutputSetResume) {
+        newOutputElement.id = `${newOutputElement.id}-${counter}`;
+        newOutputElement.textContent = "";
+    }
+
+    newOutputSet.getElementsByClassName("comma")[0].classList.add("hidden");
+    newOutputSet.getElementsByClassName("hyphen")[0].classList.add("hidden");
+    experienceOutput.appendChild(newOutputSet);
 }
 
 function updateResume(element) {
-  const value = localStorage.getItem(element.name);
-  document.getElementById(element.name).textContent = value;
+    const value = localStorage.getItem(element.name);
+
+    const outPutParentElement = document.getElementById(element.name).parentNode
+    if (element.name.startsWith("cv-position")) {
+        if (element.value) {
+            outPutParentElement.getElementsByClassName("comma")[0].classList.remove("hidden");
+        } else {
+            outPutParentElement.getElementsByClassName("comma")[0].classList.add("hidden");
+        }
+    }
+
+    if (element.name.startsWith("cv-end-work")) {
+        if (element.value) {
+            outPutParentElement.getElementsByClassName("hyphen")[0].classList.remove("hidden");
+        } else {
+            outPutParentElement.getElementsByClassName("hyphen")[0].classList.add("hidden");
+        }
+    }
+
+    document.getElementById(element.name).textContent = value;
 }
+
+
+
